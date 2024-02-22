e466cc87e506a2307e3f6080416d13f762563d54
mharoon-dev <haroon.aghani@gmail.com>
2024-02-22 17:16:40 +0500

    counter with range

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import "./counter.css";
 import plus from "../assets/plus.png";
 import minus from "../assets/minus.png";
 // import './counter.css'
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(50);
 
   return (
     <>
@@ -27,29 +28,51 @@ function Counter() {
           {count}
         </h1>
 
-        <div className="d-flex justify-content-around align-items-center w-100 gap-2">
-          <img src={plus} width="50px" onClick={() => setCount(count + 1)} />
-          <button 
-          onClick={() => setCount(0)}
-            style={{
-              padding: "13px  18px",
-              boxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
-              webkitBoxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
-              mozBoxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
-            }}
-          >
-            <h6
+        <div className="d-flex flex-column justify-content-around align-items-center w-100 gap-2 mt-2">
+
+          {/* range slider */}
+          <input
+            onChange={() => setCount(document.getElementById("myRange").value)}
+            type="range"
+            min="1"
+            max="100"
+            value={count}
+            class="slider"
+            id="myRange"
+          />
+
+          {/* buttons div */}
+          <div className="d-flex justify-content-around align-items-center gap-2 mt-2 ">
+
+            {/* plus btn */}
+            <img src={plus} width="50px" onClick={() => setCount(count < 100 ? count + 1 : count)} />
+
+            {/* reset button */}
+            <button
+              onClick={() => setCount(0)}
               style={{
-                padding: 0,
-                margin: 0,
-                fontFamily: "cursive",
-                fontWeight: 700,
+                padding: "13px  18px",
+                boxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
+                webkitBoxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
+                mozBoxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
               }}
             >
-              Reset
-            </h6>
-          </button>
-          <img src={minus} width="50px" onClick={() => setCount(count - 1)} />
+              <h6
+                style={{
+                  padding: 0,
+                  margin: 0,
+                  fontFamily: "cursive",
+                  fontWeight: 700,
+                }}
+              >
+                Reset
+              </h6>
+            </button>
+
+            {/* minus */}
+            <img src={minus} width="50px" onClick={() => setCount(count > 0 ? count - 1 : count)} />
+
+          </div>
         </div>
       </div>
     </>
