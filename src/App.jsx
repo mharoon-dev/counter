aedf4e6bc0c40648c1cd6b773f96f0988744d142
mharoon-dev <haroon.aghani@gmail.com>
2024-02-22 16:45:13 +0500

    basic counter done

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import Counter from './Components/Counter.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Counter />
   )
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,36 +1,59 @@
-import { useState } from 'react'
-import plus from "../assets/plus.png"
-import minus from "../assets/minus.png"
+import { useState } from "react";
+import plus from "../assets/plus.png";
+import minus from "../assets/minus.png";
 // import './counter.css'
 
 function Counter() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className="d-flex flex-column justify-content-center align-items-center">
-        <h1 style={{
-            color:"white",
-            fontFamily:"cursive",
-            }}>
-            Counter
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h1
+          style={{
+            color: "white",
+            fontFamily: "cursive",
+          }}
+        >
+          Counter
         </h1>
 
-        <h1 style={{
-            color:"white",
-            fontFamily:"cursive",
-            }}>{count}</h1>
+        <h1
+          style={{
+            color: "white",
+            fontFamily: "cursive",
+          }}
+        >
+          {count}
+        </h1>
 
-        <div className="d-flex justify-content-around align-items-center w-100">
-        <img  src={plus} width="50px" onClick={() => setCount(count + 1)}  />
-        <img  src={minus} width="50px" onClick={() => setCount(count - 1)}  />
+        <div className="d-flex justify-content-around align-items-center w-100 gap-2">
+          <img src={plus} width="50px" onClick={() => setCount(count + 1)} />
+          <button 
+          onClick={() => setCount(0)}
+            style={{
+              padding: "13px  18px",
+              boxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
+              webkitBoxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
+              mozBoxShadow: "0px 3px 1px 0px rgba(229,229,229,0.73)",
+            }}
+          >
+            <h6
+              style={{
+                padding: 0,
+                margin: 0,
+                fontFamily: "cursive",
+                fontWeight: 700,
+              }}
+            >
+              Reset
+            </h6>
+          </button>
+          <img src={minus} width="50px" onClick={() => setCount(count - 1)} />
         </div>
-
-
-
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Counter
+export default Counter;
